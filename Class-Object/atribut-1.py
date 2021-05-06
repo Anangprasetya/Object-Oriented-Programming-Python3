00c6049e28e9af6f224a527be0173ec1f428afef
Anang <tyoandini@gmail.com>
2021-05-06 08:33:24 +0700

    membuat file atribut.py

---
+++ Class-Object/atribut-1.py
@@ -0,0 +1,36 @@
+# Kita gunakan class Rumah
+class Rumah:
+
+	# atribut
+	index = 0
+
+	# setiap method harus memiliki paramter self
+	# method __init__ sebagai Constructor
+	def __init__(self, pemilik):
+		self.pemilik = pemilik
+		Rumah.index += 1
+
+	# method pada umumnya
+	def katakan(self):
+		print("Selamat datang di rumah",self.pemilik)
+
+	def RumahKeBerapa(self):
+		# print("Rumah ",self.pemilik, "berada di urutan ", Rumah.index, "\n")
+		print(f'Rumah {self.pemilik} berada di urutan {Rumah.index} \n')
+
+
+
+
+
+# Inisialisasi object
+anang = Rumah("Anang")
+anang.katakan()
+anang.RumahKeBerapa()
+
+dwi = Rumah("Dwi")
+dwi.katakan()
+dwi.RumahKeBerapa()
+
+bayu = Rumah("Bayu")
+bayu.katakan()
+bayu.RumahKeBerapa()
