e384e1abccac8f57ac5efe11c97859fbd9d455d5
Anang <tyoandini@gmail.com>
2021-05-06 08:23:50 +0700

    file class object

---
+++ Class-Object/object-1.py
@@ -0,0 +1,22 @@
+class Rumah:
+	# setiap method harus memiliki paramter self
+
+	# method __init__ sebagai Constructor
+	def __init__(self, pemilik):
+		self.pemilik = pemilik
+
+	# method pada umumnya
+	def katakan(self):
+		print("Selamat datang di rumah",self.pemilik)
+
+
+
+
+
+# Inisialisasi object
+anang = Rumah("Anang")
+anang.katakan()
+
+
+bayu = Rumah("Bayu")
+bayu.katakan()
