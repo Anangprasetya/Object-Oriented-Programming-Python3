3c505c3e69273c234b837db184b5d6c4fd3fd2dc
Anang <tyoandini@gmail.com>
2021-05-07 09:11:48 +0700

    update atribut

---
+++ Class-Object/update-atribut-1.py
@@ -0,0 +1,24 @@
+
+class NamaSaya:
+	def __init__(self):
+		self.namaku = "Dwi"
+
+	def ubahnama(self):
+		self.namaku = "Anang"
+
+
+a = NamaSaya()
+print(a.namaku)
+
+a.ubahnama()
+print(a.namaku)
+
+
+
+
+"""
+==========================================================
+==================  Versi Python 3.8.5  ==================
+==================        2021          ==================
+==========================================================
+"""
